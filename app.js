1f9a50b779ba89fed16c30510f96169f197224e8
JMoneymaker <jasmin.moneymaker@gmail.com>
2019-10-05 10:52:56 -0700

    game is working still need to figure out results display

--- app.js
+++ app.js
@@ -7,42 +7,63 @@ import { checkResult } from './get-random-throw.js';
 
 const playButton = document.getElementById('play-button');
 const resetButton = document.getElementById('reset-button');
-const gameResults = document.getElementById('game-results');
-const gameStats = document.getElementById('game-stats');
 const winsSpan = document.getElementById('wins');
 const lossesSpan = document.getElementById('losses');
 const drawsSpan = document.getElementById('draws');
+const setWinLossStatement = document.getElementById('win-loss-statement');
+const computerChoiceSpan = document.getElementById('computer-throw');
+const userThrowSpan = document.getElementById('user-throw');
 
 
 // set some initial state
 
-let wins = 0;
-let losses = 0;
-let draws = 0;
+let totalWins = 0;
+let totalLosses = 0;
+let totalDraws = 0;
 
 
-//define DOM utility function
-const displayGameStats = () => {
-    winsSpan.textContent = wins;
-    lossesSpan.textContent = losses;
-    drawsSpan.textcontent = draws;
-};
-
-const resultsDisplay = (results) => {
-    if (results = 'win') {
-        return "You won!";
-   }
-}
-
-// make changes to state and dom
-
-// add event listeners
+//play game
 
 playButton.addEventListener('click', () => {
-    let userChoice = document.querySelector('input:checked').value;
+    const selectedRadioValue = document.querySelector('input:checked');
+    const userChoice = selectedRadioValue.value;
     const computerChoice = getComputerChoice();
-    const results = checkResult(userChoice, computerChoice);
-
+    const matchResults = checkResult(userChoice, computerChoice);
+    setWinLossStatement.textContent = matchResults;
+    computerChoiceSpan.textContent = computerChoice;
+    userThrowSpan.textContent = userChoice;
     
-}); 
+    if (matchResults === 'win') {
+        setWinLossStatement.textContent = 'You\'re the G.D. winner!';
+        winsSpan.textContent += 1;
+    } else if (matchResults === 'lose') {
+        setWinLossStatement.textContent = 'You\'re the G.D. loser!';
+        lossesSpan.textContent += 1;
+    } else {
+        setWinLossStatement.textContent = 'It was a G.D. draw!';
+        drawsSpan.textContent += 1;
+    }
+    
+    
+    // conditional statement checking results and changing text content of a the setWinLossStatement span depening on game result. Move 47 into each condition of the conditional statment.
+    
+});
+// make changes to state and dom
 
+/* const runMatch = () => {
+    const displayGameStats = () => {
+        winsSpan.textContent = wins;
+        lossesSpan.textContent = losses;
+        drawsSpan.textcontent = draws;
+    const radioButtonValue = document.querySelector('input:checked');
+    const userChoice = radioButtonValue.value;
+    const gameResults = checkResult(userChoice, computerChoice);
+    gameStats(userChoice, computerChoice, gameResults);
+    setWinLossStatement(gameResults);
+    */
+   
+   // add event listeners
+   
+   
+   //define DOM utility function
+   
